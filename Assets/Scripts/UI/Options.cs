a7377e0828ecd598a3ecbe76f82cf5ff821e8a3a
Supergaming <ilikeaustin2008@gmail.com>
2023-07-15 21:00:48 +1000

    Added a scaffold of the main menu, and also audio mixers

--- Assets/Scripts/UI/Options.cs
+++ Assets/Scripts/UI/Options.cs
@@ -1,13 +1,28 @@
-﻿using System.Collections; 
+﻿using System;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class Options : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
-    
-    public void VolumeSlider(float volume)
+
+    public void MasterVolumeSlider(float volume)
     {
-        audioMixer.SetFloat("volume", volume);
+        audioMixer.SetFloat("masterVolume", volume);
+    }
+
+    public void MusicVolumeSlider(float volume)
+    {
+        audioMixer.SetFloat("musicVolume", volume);
+    }
+    
+    public void VoicelineVolumeSlider(float volume)
+    {
+        audioMixer.SetFloat("voicelineVolume", volume);
+    }
+    
+    public void FXVolumeSlider(float volume)
+    {
+        audioMixer.SetFloat("fxVolume", volume);
     }
 }
