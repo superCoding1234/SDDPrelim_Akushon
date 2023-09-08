68f50a81f3544d635381f6960904e3338c95e895
Supergaming <ilikeaustin2008@gmail.com>
2023-09-08 15:04:48 +1000

    Big Overhaul

---
+++ Assets/Scripts/Abilities/StandardAbility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public enum AbilityState
+{
+    ready,
+    active,
+    cooldown
+}
+
+public enum AbilityType
+{
+    elemental,
+    ultimate
+}
+
+public class StandardAbility : ScriptableObject
+{
+    public AbilityType abilityType;
+    public float cooldownTime;
+    public float activeTime;
+    [NonSerialized] public AbilityState abilityState = AbilityState.ready;
+    
+    public virtual void Activate(GameObject parent) {}
+    public virtual void WhileActive(GameObject parent) {}
+    public virtual void Deactivate(GameObject parent) {}
+}
