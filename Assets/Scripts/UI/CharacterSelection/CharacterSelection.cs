5cf891cbb32ac5f3585719241d7f1216478cba0c
Supergaming <ilikeaustin2008@gmail.com>
2023-09-09 23:18:55 +1000

    STUFF

---
+++ Assets/Scripts/UI/CharacterSelection/CharacterSelection.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [SerializeField] private Sprite[] characters;
+    [SerializeField] private Sprite[] elements;
+    private int characterCounter;
+    private int elementCounter;
+    private Image im;
+
+    public void CycleForwardCharacter(GameObject image)
+    {
+        im = image.GetComponent<Image>();
+        im.sprite = characters[Mod(++characterCounter, 3)];
+    }
+
+    public void CycleBackwardCharacter(GameObject image)
+    {
+        im = image.GetComponent<Image>();
+        im.sprite = characters[Mod(--characterCounter, 3)];
+    }
+
+    private int Mod(int x, int y)
+    {
+        return (x % y + y) % y;
+    }
+}
